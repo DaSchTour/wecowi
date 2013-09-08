d145d379a76482ea7da131f57fd700c00638def8
DaSch <dasch@daschmedia.de>
2013-09-08 13:20:31 +0200

    added option for on off social links

--- WeCoWi.php
+++ WeCoWi.php
@@ -8,7 +8,7 @@ $wgExtensionCredits['other'][] = array (
 'url'=>'https://github.com/DaSchTour/wecowi',
 'description'=>'Spezielle Modifikationen der MediaWiki-Software und ihrer Erweiterungen zur besseren Nutzbarkeit.',
 'author'=>'[http://www.dasch-tour.de DaSch]',
-'version'=>'0.8',
+'version'=>'0.8.5',
 );
 /** EXTENSION */
 $dir = dirname(__FILE__);
@@ -16,15 +16,9 @@ $wgExtensionMessagesFiles['WeCoWi'] = $dir . '/WeCoWi.i18n.magic.php';
 
 $wgAutoloadClasses['WeCoWiHooks'] = "$dir/WeCoWi.hooks.php";
 
-$wgPiwikIDSite = "";
-$wgPiwikURL = "";
-$wgPiwikIgnoreSysops = true;
-$wgPiwikIgnoreBots = true;
-$wgPiwikCustomJS = "";
-$wgPiwikUsePageTitle = false;
-$wgPiwikActionName = "";
-
 $wgHooks['ParserGetVariableValueSwitch'][] = 'WeCoWiHooks::myVars'; 
-$wgHooks['SkinBuildSidebar'][] = 'WeCoWiHooks::SocialSidebar';
-$wgHooks['BeforePageDisplay'][] = 'WeCoWiHooks::SocialSidebarScripts';
 $wgHooks['MagicWordwgVariableIDs'][] = 'WeCoWiHooks::wfMyDeclareVarIds';
+if ($wgWeCoWiSocialSidebar) {
+	$wgHooks['SkinBuildSidebar'][] = 'WeCoWiHooks::SocialSidebar';
+	$wgHooks['BeforePageDisplay'][] = 'WeCoWiHooks::SocialSidebarScripts';;
+}
